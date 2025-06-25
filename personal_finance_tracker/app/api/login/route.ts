ffd70620426751fc05ed7364b6212f1fc4e0748f
Rafid Elahi <rafidelahi.dev@gmail.com>
2025-06-25 18:38:16 +0800

    Still finished log in

--- personal_finance_tracker/app/api/login/route.ts
+++ personal_finance_tracker/app/api/login/route.ts
@@ -1,6 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 import bcrypt from 'bcrypt'
-import { error } from "console";
+import jwt from 'jsonwebtoken';
+import { cookies } from "next/headers";
 
 const prisma = new PrismaClient();
 
@@ -8,19 +9,35 @@ export async function POST(req:Request) {
     const {email, password} = await req.json();
 
     try{
+        console.log('[LOGIN] Received:', { email });
         const user = await prisma.user.findUnique({where: {email}});
 
         if(!user){
+            console.log('[LOGIN] User not found');
             return new Response(JSON.stringify({error: 'Email not found'}), {status: 404});
         }
 
         const isValid = await bcrypt.compare(password, user.password);
 
         if(!isValid){
+            console.log('[LOGIN] Password incorrect');
             return new Response(JSON.stringify({error: 'Password is incorrect'}), {status: 401});
         }
 
-        return new Response(JSON.stringify({success: true}), {status: 200});
+        const token = jwt.sign(
+            {userId: user.id, email: user.email},
+            process.env.JWT_SECRET!,
+            {expiresIn: '5m'}
+        )
+
+        console.log('[LOGIN] Token created');
+
+        return new Response(JSON.stringify({success: true}), {
+            status: 200,
+            headers: {
+                'Set-Cookie': `token=${token}; httpOnly; Path=/; Max-Age=300; SameSite=Stict`,
+            },
+        });
     }catch(error){
         console.error(error);
         return new Response(JSON.stringify({error: 'Server Error'}), {status: 500})
--- personal_finance_tracker/app/finance/page.tsx
+++ personal_finance_tracker/app/finance/page.tsx
@@ -1,8 +1,24 @@
-'use client'
+import { cookies } from "next/headers"
+import jwt from 'jsonwebtoken';
 
-export default function Page(){
 
-    return (
-        <div>Hello world</div>
-    )
+export default async function FinancePage(){
+    const cookieStore = await cookies();
+    const token = cookieStore.get('token')?.value;
+
+    if(!token){
+        return <div className="p-10 text-red-600">Unauthorized: No session</div>
+    }
+
+    try{
+        const user = jwt.verify(token, process.env.JWT_SECRET!);
+        return(
+            <div className="p-10">
+                <h1 className="text-3xl font-bold">Welcome to the Finance Page</h1>
+                <p>User Email: {(user as any).email}</p>
+            </div>
+            );
+        } catch (error) {
+            return <div className="p-10 text-red-600">Invalid or expired token</div>;
+        }
 }
