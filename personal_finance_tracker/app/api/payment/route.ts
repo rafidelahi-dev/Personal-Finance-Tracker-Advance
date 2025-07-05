a7de70385093602c3d5f002a8a4c21f5b953f9ad
Rafid Elahi <rafidelahi.dev@gmail.com>
2025-07-05 11:19:45 +0800

    successfully add payment

---
+++ personal_finance_tracker/app/api/payment/route.ts
@@ -0,0 +1,24 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export async function POST(req:Request) {
+    const {amount, description, date, category, accountId} = await req.json();
+
+    try{
+        await prisma.payment.create({
+            data: {
+                amount: parseFloat(amount),
+                description,
+                date: new Date(date),
+                category,
+                accountId,
+            },
+        });
+
+        return new Response(JSON.stringify({success: true}), {status: 201});
+    }catch(error){
+        console.error(error);
+        return new Response(JSON.stringify({error: 'Failed to add payment'}), {status: 500});
+    }
+}
